0f391093b9886837ad6f82e45d220908d23bc2cc
Raistlin <stilmor@gmail.com>
2024-11-04 02:50:21 +0100

    unit test movimiento

--- src/movimiento/movimiento.controller.spec.ts
+++ src/movimiento/movimiento.controller.spec.ts
@@ -1,18 +1,56 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovimientoController } from './movimiento.controller';
+import { MovimientoService } from './movimiento.service';
+import { Movimiento } from '../models/movimiento.model';
 
 describe('MovimientoController', () => {
   let controller: MovimientoController;
+  let movimientoService: MovimientoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MovimientoController],
+      providers: [
+        {
+          provide: MovimientoService,
+          useValue: {
+            findByCuentaId: jest.fn(),
+            createMovimiento: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<MovimientoController>(MovimientoController);
+    movimientoService = module.get<MovimientoService>(MovimientoService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should call findByCuentaId with correct numeroCuenta', async () => {
+    const numeroCuenta = '123456';
+
+    const mockMovimientos = [
+      {
+        id: 1,
+        tipo: 'ingreso',
+        monto: 100,
+        fecha: new Date(),
+        cuentaId: 1,
+      },
+    ] as Movimiento[];
+
+    jest
+      .spyOn(movimientoService, 'findByCuentaId')
+      .mockResolvedValue(mockMovimientos);
+
+    const result = await controller.findByCuentaId(numeroCuenta);
+
+    expect(movimientoService.findByCuentaId).toHaveBeenCalledWith(
+      +numeroCuenta,
+    );
+    expect(result).toEqual(mockMovimientos);
   });
 });
--- src/movimiento/movimiento.service.spec.ts
+++ src/movimiento/movimiento.service.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MovimientoService } from './movimiento.service';
+import { getModelToken } from '@nestjs/sequelize';
+import { Movimiento } from '../models/movimiento.model';
+import { BadRequestException } from '@nestjs/common';
 
 describe('MovimientoService', () => {
   let service: MovimientoService;
+  let movimientoModel: typeof Movimiento;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [MovimientoService],
+      providers: [
+        MovimientoService,
+        {
+          provide: getModelToken(Movimiento),
+          useValue: {
+            findAll: jest.fn(),
+            create: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     service = module.get<MovimientoService>(MovimientoService);
+    movimientoModel = module.get<typeof Movimiento>(getModelToken(Movimiento));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('debería obtener movimientos de una cuenta', async () => {
+    const movimientosMock: Partial<Movimiento>[] = [
+      { tipo: 'ingreso', monto: 100 } as Movimiento,
+    ];
+    (movimientoModel.findAll as jest.Mock).mockResolvedValue(movimientosMock);
+
+    const result = await service.findByCuentaId(1);
+
+    expect(result).toEqual(movimientosMock);
+    expect(movimientoModel.findAll).toHaveBeenCalledWith({
+      where: { cuentaId: 1 },
+    });
+  });
+
+  it('debería lanzar un error si el monto es negativo al crear un movimiento', async () => {
+    const mockMovimiento = jest.spyOn(movimientoModel, 'create');
+    mockMovimiento.mockImplementation(() => {
+      throw new BadRequestException('El monto debe ser positivo');
+    });
   });
 });
