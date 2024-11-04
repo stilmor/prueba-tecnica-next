3899414a210542141948a58b35815151bcbd0f44
Raistlin <stilmor@gmail.com>
2024-11-04 02:16:00 +0100

    adapt app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,21 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import sequelizeConfig from './config/sequelize.config';
+import { CuentaModule } from './cuenta/cuenta.module';
+import { TransferenciaModule } from './transferencia/transferencia.module';
+import { TarjetaModule } from './tarjeta/tarjeta.module';
+import { MovimientoModule } from './movimiento/movimiento.module';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [SequelizeModule.forRoot(sequelizeConfig)],
+  imports: [
+    SequelizeModule.forRoot(sequelizeConfig),
+    CuentaModule,
+    TransferenciaModule,
+    TarjetaModule,
+    MovimientoModule,
+    ClienteModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
